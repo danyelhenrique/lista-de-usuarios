d8c98e6bf6561c43975b6c486f137794a70f3538
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-07-11 22:31:37 -0300

    create scanfol project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Latest Release</span>
+          <!-- <v-icon>mdi-open-in-new</v-icon> -->
+        </v-btn>
+        <span class="group pa-2">
+          <i class="material-icons">search</i>
+          <i class="material-icons">home</i>
+          <i class="material-icons">power_settings_new</i>
+          <i class="material-icons">security</i>
+          <i class="material-icons">person</i>
+          <i class="material-icons">settings</i>
+
+          <img :src="filter" alt="filter" class="filter-icon" />
+          <v-icon>mdi-info</v-icon>
+        </span>
+      </div>
+    </v-app-bar>
+
+    <v-content> </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  mdiHome,
+  mdiPower,
+  mdiSecurity,
+  mdiAccount,
+  mdiSelectSearch,
+} from "@mdi/js";
+import filter from "@/assets/filter.svg";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "aa",
+  data: () => ({
+    mdiHome,
+    mdiPower,
+    mdiSecurity,
+    mdiAccount,
+    mdiSelectSearch,
+    filter,
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+img.filter-icon {
+  height: 24px;
+  width: 24px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
 
-Vue.config.productionTip = false
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
