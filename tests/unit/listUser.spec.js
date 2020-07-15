c551974ee4c703c5ad7cc95b71313a5a70972d18
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-07-15 15:13:15 -0300

    test: change test to mock vuex user list

--- tests/unit/listUser.spec.js
+++ tests/unit/listUser.spec.js
@@ -1,16 +1,51 @@
 import vuetify from "vuetify";
+import Vuex from "vuex";
 
 import { mount, createLocalVue } from "@vue/test-utils";
 
 import ListUsers from "@/components/ListUser";
 
 const localVue = createLocalVue();
+localVue.use(Vuex);
 localVue.use(vuetify);
-
+const mockUsers = [
+  {
+    name: "Eclair",
+    email: "test@gmail.com",
+    created_at: "30/05/2020",
+    rules: "01",
+    updated_at: "30/05/2020",
+    status: true,
+    actions: "...",
+  },
+  {
+    name: "Cupcake",
+    email: "test@gmail.com",
+    created_at: "30/05/2020",
+    rules: "01",
+    updated_at: "30/05/2020",
+    status: true,
+    actions: "...",
+  },
+];
 describe("ListUsers.vue", () => {
   const mountFunction = (options) => {
+    const users = {
+      state: {
+        users: mockUsers,
+      },
+      mutations: [],
+      actions: {},
+    };
+
+    const nstore = new Vuex.Store({
+      modules: {
+        users,
+      },
+    });
     return mount(ListUsers, {
       localVue,
+      store: nstore,
       ...options,
     });
   };
@@ -28,32 +63,7 @@ describe("ListUsers.vue", () => {
   });
 
   it("should be able to list correct length users", async () => {
-    const mockUsers = [
-      {
-        name: "Eclair",
-        email: "test@gmail.com",
-        created_at: "30/05/2020",
-        rules: "01",
-        updated_at: "30/05/2020",
-        status: true,
-        actions: "...",
-      },
-      {
-        name: "Cupcake",
-        email: "test@gmail.com",
-        created_at: "30/05/2020",
-        rules: "01",
-        updated_at: "30/05/2020",
-        status: true,
-        actions: "...",
-      },
-    ];
-
-    const propsData = {
-      users: mockUsers,
-    };
     const wrapper = mountFunction();
-    await wrapper.setData(propsData);
     const listUser = wrapper.findAll(".list-user");
 
     expect(listUser.length).toEqual(mockUsers.length);
